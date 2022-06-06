0c1946864e2bef0db0a4a8dff3ba72395bd5403d
arkord <arkord1@gmail.com>
2022-06-06 15:07:21 -0500

    shared preferences

--- lib/screens/home_screeen.dart
+++ lib/screens/home_screeen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,8 +10,29 @@ class HomeScreen extends StatefulWidget{
 }
 
 class _HomeScreenState extends State<HomeScreen>{
+  final controllerAmount = TextEditingController();
+  final controllerPercent = TextEditingController();
+  double tip = 0;
+
+  void setDefaultValues(double value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('percent', value);
+  }
+
+  Future<double?> getDefaultValues() async {
+    final prefs = await SharedPreferences.getInstance();
+    final double percent = prefs.getDouble('percent') ?? 15;
+
+    return percent;
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDefaultValues().then((value) => {
+      controllerPercent.text = value.toString()
+    }); 
+
     return Scaffold(
       appBar: AppBar(title: const Text("Tips"),),
       body:Center(
@@ -19,6 +41,7 @@ class _HomeScreenState extends State<HomeScreen>{
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
               child: TextFormField(
+                controller: controllerAmount,
                 decoration: const InputDecoration(
                   labelText: "Amount",
                   border: UnderlineInputBorder()
@@ -28,17 +51,22 @@ class _HomeScreenState extends State<HomeScreen>{
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
               child: TextFormField(
+                controller: controllerPercent,
                 decoration: const InputDecoration(
                   labelText: "Percent",
                   border: UnderlineInputBorder()
                 ),
               ),
             ),
-            const Text("Tip to pay"),
+            Text("Tip to pay \$$tip"),
             ElevatedButton(
               onPressed: () {
                 setState(() {
+                  double amount = double.parse(controllerAmount.text);
+                  double percent = double.parse(controllerPercent.text);
                   
+                  tip = amount * percent / 100;
+                  setDefaultValues(percent);
                 });
               }, 
               child:const Text("Calculate")
